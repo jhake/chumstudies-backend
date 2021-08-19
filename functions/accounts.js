5c264971d08387d16c0d0de3c2da1ef946948902
jhake <jhake.aquino123@gmail.com>
2021-08-19 20:39:31 +0800

    Revert "comment out accounts.js"
    This reverts commit c16ee30e1f992b51be5f00bf2b02850e6145c4f7.

--- functions/accounts.js
+++ functions/accounts.js
@@ -1,45 +1,45 @@
-// const { AccountsModule } = require("@accounts/graphql-api");
-// const mongoose = require("mongoose");
-// const { Mongo } = require("@accounts/mongo");
-// const { AccountsServer } = require("@accounts/server");
-// const { AccountsPassword } = require("@accounts/password");
+const { AccountsModule } = require("@accounts/graphql-api");
+const mongoose = require("mongoose");
+const { Mongo } = require("@accounts/mongo");
+const { AccountsServer } = require("@accounts/server");
+const { AccountsPassword } = require("@accounts/password");
 
-// // We connect mongoose to our local mongodb database
-// mongoose.connect(process.env.MONGODB_CONNECTION, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// We connect mongoose to our local mongodb database
+mongoose.connect(process.env.MONGODB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// // We tell accounts-js to use the mongo connection
-// const accountsMongo = new Mongo(mongoose.connection);
+// We tell accounts-js to use the mongo connection
+const accountsMongo = new Mongo(mongoose.connection);
 
-// const accountsPassword = new AccountsPassword({
-//   // This option is called when a new user create an account
-//   // Inside we can apply our logic to validate the user fields
-//   validateNewUser: (user) => {
-//     // For example we can allow only some kind of emails
-//     if (user.email.endsWith(".xyz")) {
-//       throw new Error("Invalid email");
-//     }
+const accountsPassword = new AccountsPassword({
+  // This option is called when a new user create an account
+  // Inside we can apply our logic to validate the user fields
+  validateNewUser: (user) => {
+    // For example we can allow only some kind of emails
+    if (user.email.endsWith(".xyz")) {
+      throw new Error("Invalid email");
+    }
 
-//     return { ...user, isTeacher: false };
-//   },
-// });
+    return { ...user, isTeacher: false };
+  },
+});
 
-// const accountsServer = new AccountsServer(
-//   {
-//     // We link the mongo adapter to the server
-//     db: accountsMongo,
-//     // Replace this value with a strong random secret
-//     tokenSecret: process.env.TOKEN_SECRET,
-//   },
-//   {
-//     // We pass a list of services to the server, in this example we just use the password service
-//     password: accountsPassword,
-//   }
-// );
+const accountsServer = new AccountsServer(
+  {
+    // We link the mongo adapter to the server
+    db: accountsMongo,
+    // Replace this value with a strong random secret
+    tokenSecret: process.env.TOKEN_SECRET,
+  },
+  {
+    // We pass a list of services to the server, in this example we just use the password service
+    password: accountsPassword,
+  }
+);
 
-// // We generate the accounts-js GraphQL module
-// const accountsGraphQL = AccountsModule.forRoot({ accountsServer });
+// We generate the accounts-js GraphQL module
+const accountsGraphQL = AccountsModule.forRoot({ accountsServer });
 
-module.exports = null;
+module.exports = accountsGraphQL;
