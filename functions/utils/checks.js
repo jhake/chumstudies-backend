6d934a3224247fb9cdd5a315a4b77a3182e528f9
jhake <jhake.aquino123@gmail.com>
2021-09-12 17:02:46 +0800

    add membership checks

--- functions/utils/checks.js
+++ functions/utils/checks.js
@@ -1,5 +1,32 @@
 const { AuthenticationError } = require("apollo-server-lambda");
 
+const CourseStudent = require("../models/courseStudent.js");
+const GroupStudent = require("../models/groupStudent.js");
+const Course = require("../models/course.js");
+
 module.exports.loginCheck = (context) => {
   if (!context.user) throw new AuthenticationError("you must be logged in");
+};
+
+module.exports.isCourseTeacher = async (teacherId, courseId) => {
+  const course = await Course.findById(courseId);
+  return course.teacher === teacherId;
+};
+
+module.exports.isCourseStudent = async (studentId, courseId) => {
+  const courseStudent = await CourseStudent.findOne({
+    course: courseId,
+    student: studentId,
+  });
+
+  return !!courseStudent;
+};
+
+module.exports.isGroupStudent = async (studentId, groupId) => {
+  const groupStudent = await GroupStudent.findOne({
+    group: groupId,
+    student: studentId,
+  });
+
+  return !!groupStudent;
 };
