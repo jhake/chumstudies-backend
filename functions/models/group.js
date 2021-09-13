44eff057957c84df459d97bd0f278fc5a3c70b31
jhake <jhake.aquino123@gmail.com>
2021-09-14 03:16:15 +0800

    add groupcode generation

--- functions/models/group.js
+++ functions/models/group.js
@@ -1,10 +1,18 @@
 const { Schema, model } = require("mongoose");
 
+const generateRandomString = require("../utils/generateRandomString.js");
+
 module.exports = model(
   "Group",
   Schema({
     name: String,
-    groupCode: String,
+    groupCode: {
+      type: String,
+      default: function () {
+        const { name } = this;
+        return `${name.replace(/\s+/g, "")}-${generateRandomString(10)}`;
+      },
+    },
     isActive: { type: Boolean, default: true },
     course: { type: Schema.Types.ObjectId, ref: "Course" },
   })
