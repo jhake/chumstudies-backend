d72aa357cbd3b52f735a6390ff280428dd31f2ce
jhake <jhake.aquino123@gmail.com>
2021-09-18 02:50:48 +0800

    fix typo

--- functions/models/notification.js
+++ functions/models/notification.js
@@ -6,8 +6,8 @@ module.exports = model(
     date: Date,
     content: String,
     user: { type: Schema.Types.ObjectID, ref: "User" },
-    course: { type: Schema.Type.ObjectID, ref: "Course" },
-    post: { type: Schema.Type.ObjectID, ref: "Post" },
-    agenda: { type: Schema.Type.ObjectID, ref: "Agenda" },
+    course: { type: Schema.Types.ObjectID, ref: "Course" },
+    post: { type: Schema.Types.ObjectID, ref: "Post" },
+    agenda: { type: Schema.Types.ObjectID, ref: "Agenda" },
   })
 );
