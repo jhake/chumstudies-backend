ade298295b55153a235c9d25dd2a8acddcd16ca2
jhake <jhake.aquino123@gmail.com>
2021-09-17 17:08:13 +0800

    Update accounts.js

--- functions/accounts.js
+++ functions/accounts.js
@@ -6,8 +6,8 @@ const { AccountsPassword } = require("@accounts/password");
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
