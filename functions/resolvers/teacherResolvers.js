4f2ab3c192c2a17f5700c075d97e89b25dfd3e1f
jhake <jhake.aquino123@gmail.com>
2021-09-18 03:23:19 +0800

    make others' group and courses private.

--- functions/resolvers/teacherResolvers.js
+++ functions/resolvers/teacherResolvers.js
@@ -3,7 +3,8 @@ const { Course, User } = require("../models/index.js");
 module.exports = {
   Teacher: {
     user: async (teacher) => await User.findById(teacher.id),
-    courses: async (teacher) => {
+    courses: async (teacher, _, context) => {
+      if (context.user.id !== teacher.id) throw Error("can't query other's courses");
       const filter = { teacher };
 
       return {
