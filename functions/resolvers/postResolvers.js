1f1c0f46c330017010c16b740a1f73000497999e
jhake <jhake.aquino123@gmail.com>
2021-09-28 19:46:30 +0800

    add activity and groupactivity to post resolvers and type

--- functions/resolvers/postResolvers.js
+++ functions/resolvers/postResolvers.js
@@ -1,11 +1,13 @@
 const { validateAttachment, destroy } = require("../utils/cloudinary");
 
-const { Post, User } = require("../models/index.js");
+const { Post, User, Activity, GroupActivity } = require("../models/index.js");
 const { loginCheck, isCourseStudent, isGroupStudent, isCourseTeacher } = require("../utils/checks");
 
 module.exports = {
   Post: {
     user: async (post) => await User.findById(post.user),
+    activity: async (post) => await Activity.findById(post.activity),
+    groupActivity: async (post) => await GroupActivity.findById(post.groupActivity),
   },
 
   Query: {
@@ -41,6 +43,8 @@ module.exports = {
 
       const post = new Post({
         ...args,
+        course: courseId,
+        group: groupId,
         user: context.user.id,
       });
 
