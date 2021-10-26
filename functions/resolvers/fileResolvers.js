0bc9b113e71fe809572bb7a783fa58289b068560
nclblrs <63730929+nclblrs@users.noreply.github.com>
2021-10-26 16:23:29 +0800

    added study group file resolver (#57)

--- functions/resolvers/fileResolvers.js
+++ functions/resolvers/fileResolvers.js
@@ -1,5 +1,5 @@
 const { Post, User, Activity, GroupActivity, Course } = require("../models/index.js");
-const { loginCheck, isCourseStudent } = require("../utils/checks");
+const { loginCheck, isCourseStudent, isGroupStudent } = require("../utils/checks");
 
 module.exports = {
   File: {
@@ -23,6 +23,21 @@ module.exports = {
         postFiles: await Post.find(filter).sort({ _id: -1 }),
         activityFiles: await Activity.find(filter).sort({ _id: -1 }),
         groupActivityFiles: await GroupActivity.find(filter).sort({ _id: -1 }),
+      };
+    },
+
+    studyGroupFiles: async (_, { groupId }, context) => {
+      loginCheck(context);
+
+      const userId = context.user.id;
+
+      const inGroup = await isGroupStudent(userId, groupId);
+      if (!inGroup) throw Error("not in group");
+
+      const filter = { group: groupId, attachment: { $exists: true } };
+
+      return {
+        postFiles: await Post.find(filter).sort({ _id: -1 }),
       };
     },
   },
